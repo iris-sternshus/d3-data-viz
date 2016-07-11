5f4953d4d2682efd0ee9c07539c54a0e0b7ffa20
iris-sternshus <iris@sternshus.com>
2016-07-11 22:23:28 +0000

    Added d3 code for graph.

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-d3.selectAll("p").style("color", function() {
-  return "hsl(" + Math.random() * 360 + ",100%,50%)";
-});
-
-
-var vis = d3.select("#graph")
-            .append("svg")
-.attr("width", 200).attr("height", 200); // width and height of the svg graph area
-
+// data
 var nodes = [
     {x: 10, y: 50},
     {x: 70, y: 10},
@@ -14,11 +6,43 @@ var nodes = [
     {x: 100, y: 11}
   ]
 
-vis.selectAll("circle.nodes")
-   .data(nodes) // the data is from the variable nodes, which we defined above
+var padding = 30;
+
+var svg = d3.select("#graph")
+            .append("svg")
+            .attr("width", 500).attr("height", 500); // width and height of the svg graph area
+
+// see : http://alignedleft.com/tutorials/d3/drawing-svgs for details
+
+
+// creating scale variables
+var xScale = d3.scaleLinear().domain([0, 200]).range([padding, 500 - padding * 2]);;
+var yScale = d3.scaleLinear().domain([0, 200]).range([500 - padding, padding]);
+
+var xAxis = d3.axisBottom().scale(xScale).tickValues([0, 50, 100, 150]);
+var yAxis = d3.axisLeft().scale(yScale).tickValues([0, 50, 100, 150]);
+
+// see: http://alignedleft.com/tutorials/d3/axes for details
+
+svg.selectAll("circle")
+   .data(nodes) // data from var nodes.
    .enter()
-   .append("svg:circle")
-   .attr("cx", function(d) { return d.x; })
-   .attr("cy", function(d) { return d.y; })
+   .append("circle")
+   .attr("cx", function(d) { return xScale(d.x); }) // circles need cx, cy, and r
+   .attr("cy", function(d) { return yScale(d.y); }) 
    .attr("r", "10px") // the radius of the nodes in pixels
    .attr("fill", "blue") // giving the nodes a fill color of blue
+
+   // see: http://alignedleft.com/tutorials/d3/making-a-scatterplot for details
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (500 - padding) + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
+
